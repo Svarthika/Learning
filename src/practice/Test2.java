f7dd43322545a3737bd4419f39cdf13e63a2461a
Svarthika <varthika.surthy@gmail.com>
2025-06-12 18:04:00 +0530

    First commit

---
+++ src/practice/Test2.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Test2 {
+    public static void main(String[] args) {
+        int a= 10;
+        int b = 20;
+        String first_name ="varthika";
+        String last_name="mengji";
+
+        System.out.println(first_name+last_name+a+b);
+        System.out.println(a+b+first_name+last_name);
+        System.out.println(first_name+last_name+(a+b));
+
+    }
+}
